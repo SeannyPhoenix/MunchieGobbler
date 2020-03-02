e2d50bd7ca50cba95fdd8a62f231dc5a8b7da2bd
Seanny Phoenix <seannyphoenix@gmail.com>
2020-03-02 09:42:58 -0800

    initial structuring

---
+++ js/gameLogic.js
@@ -0,0 +1,8 @@
+class MunchieGobblerGame {
+  constructor() {
+    super.constructor();
+    setTitle('Munchie Gobbler: a game by Seanny Drakon Phoenix');
+  }
+}
+
+let thisGame = new MunchieGobblerGame();
